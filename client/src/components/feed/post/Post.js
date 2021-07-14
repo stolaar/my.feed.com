372c8a26b107769322300b2c9de1d545110bb03a
Stolic <stolaaar@gmail.com>
2021-07-14 09:44:22 +0200

    Update Post card design

--- client/src/components/feed/post/Post.js
+++ client/src/components/feed/post/Post.js
@@ -1,30 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
+import {
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  CardActionArea,
+  CardMedia
+} from '@material-ui/core'
 import { red } from '@material-ui/core/colors'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ShareIcon from '@material-ui/icons/Share'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 import feed from '../../../constants/feed'
 
 const useStyles = makeStyles(theme => ({
   root: {
     maxWidth: 345,
-    maxHeight: 450,
     margin: 5
   },
   media: {
     maxHeight: 120,
     minWidth: '100%',
-    // paddingTop: '56.25%', // 16:9,
     objectFit: 'contain'
   },
   expand: {
@@ -44,10 +40,15 @@ const useStyles = makeStyles(theme => ({
     fontSize: 12
   },
   description: {
-    whiteSpace: 'nowrap',
-    maxHeight: 50,
-    maxWidth: '100%',
-    margin: 'auto'
+    height: 100,
+    fontSize: '12px'
+  },
+  title: {
+    fontSize: '16px',
+    height: 60,
+    '& :hover': {
+      textDecoration: 'none'
+    }
   }
 }))
 
@@ -63,49 +64,47 @@ export default function Post({ title, image, label, description, link }) {
   const onImageError = () => {
     setImage(feed.post.placeholder)
   }
-
   return (
     <Card className={classes.root}>
-      <CardHeader
-        className={classes.header}
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            {label}
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={title}
-        subheader="September 14, 2016"
-      />
-      <img
-        onError={onImageError}
-        className={classes.media}
-        src={postImage}
-        alt=""
-      />
-      <CardContent>
-        <Typography
-          variant="body2"
-          color="textSecondary"
-          component="p"
-          className={classes.description}
-        >
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+      <CardActionArea>
         <Link target="_blank" rel="noopener noreferrer" to={{ pathname: link }}>
-          <Button size="small">Learn More</Button>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            onError={onImageError}
+            image={postImage}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography
+              color={'textPrimary'}
+              className={classes.title}
+              gutterBottom
+              variant="h5"
+              component="h4"
+            >
+              {title}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className={classes.description}
+            >
+              {description}
+            </Typography>
+          </CardContent>
+        </Link>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Link target="_blank" rel="noopener noreferrer" to={{ pathname: link }}>
+          <Button color={'primary'} size="small">
+            Learn More
+          </Button>
         </Link>
       </CardActions>
     </Card>
